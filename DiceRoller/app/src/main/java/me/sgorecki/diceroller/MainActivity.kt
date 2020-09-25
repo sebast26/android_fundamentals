345538a91769a807bcc3fb68ca072cfe22366c6e
Sebastian Gorecki <gorecki.sebastian@gmail.com>
2020-09-25 09:10:39 +0200

    Activates onCLick on button click

--- DiceRoller/app/src/main/java/me/sgorecki/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/me/sgorecki/diceroller/MainActivity.kt
@@ -1,8 +1,10 @@
 package me.sgorecki.diceroller
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import me.sgorecki.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(6) + 1
+        binding.resultText.text = randomInt.toString()
     }
 }
