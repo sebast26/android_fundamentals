f47497561af576b4534d91c6b66bb0d9eb249d47
Sebastian Gorecki <gorecki.sebastian@gmail.com>
2020-09-27 16:22:08 +0200

    Adds image view

--- DiceRoller/app/src/main/java/me/sgorecki/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/me/sgorecki/diceroller/MainActivity.kt
@@ -1,7 +1,6 @@
 package me.sgorecki.diceroller
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import me.sgorecki.diceroller.databinding.ActivityMainBinding
 import kotlin.random.Random
@@ -17,13 +16,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
     }
 
     private fun rollDice() {
         val randomInt = Random.nextInt(6) + 1
-        binding.resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.diceImage.setImageResource(drawableResource)
     }
 }
