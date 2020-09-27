a4ac177cfb5f40caf787ef2780b7c0c694e0129d
Sebastian Gorecki <gorecki.sebastian@gmail.com>
2020-09-27 21:11:59 +0200

    Adds done button

--- AboutMe/app/src/main/java/me/sgorecki/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/me/sgorecki/aboutme/MainActivity.kt
@@ -1,11 +1,37 @@
 package me.sgorecki.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import me.sgorecki.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        binding.doneButton.setOnClickListener { addNickname(it) }
+
+        setContentView(view)
+    }
+
+    private fun addNickname(view: View) {
+        val nickNameEdit = binding.nickname
+        val nickNameText = binding.nicknameText
+        nickNameText.text = nickNameEdit.text
+
+        nickNameEdit.visibility = View.GONE
+        view.visibility = View.GONE
+        nickNameText.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
