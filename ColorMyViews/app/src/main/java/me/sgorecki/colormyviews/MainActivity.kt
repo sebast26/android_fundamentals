bc96f4185cec3b5cd10784afe25d686b62845e54
Sebastian Gorecki <gorecki.sebastian@gmail.com>
2020-09-28 21:59:13 +0200

    Adds more boxes and click handler to change colors

--- ColorMyViews/app/src/main/java/me/sgorecki/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/me/sgorecki/colormyviews/MainActivity.kt
@@ -1,11 +1,37 @@
 package me.sgorecki.colormyviews
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val boxes =
+            listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, constraintLayout)
+        boxes.forEach { it.setOnClickListener { makeColored(it) } }
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.boxOneText -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.boxTwoText -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.boxThreeText -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.boxFourText -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.boxFiveText -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
